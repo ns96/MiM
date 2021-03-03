399c8921dcb30bda0512ec646f943cec5449008f
Volodymyr Ananiev <volodymyr.ananiev@gmail.com>
2021-03-03 14:50:42 +0200

    Fix reading state of limit switches in STEPPER mode
    Original Arduino function digitalRead() also disables PWM on the input pin
    affecting timers.
    
    Signed-off-by: Volodymyr Ananiev <volodymyr.ananiev@gmail.com>

--- src/Drivers/LimitSW.c
+++ src/Drivers/LimitSW.c
@@ -1,16 +1,38 @@
 #include <Arduino.h>
+#include <stdint.h>
 #include "LimitSW.h"
-/**
-  * @brief Initialize the limit switch driver
-  * @param  None
-  * @retval None
-  */
 
 //---------------  Internal macros -------------------
-#define STEP_LimitSwitch1_Get()	digitalRead(S01_PIN)
-#define STEP_LimitSwitch2_Get()	digitalRead(S02_PIN)
+#define STEP_LimitSwitch1_Get()	digitalReadNoPWM(S01_PIN)
+#define STEP_LimitSwitch2_Get()	digitalReadNoPWM(S02_PIN)
 #define STEP_LIMIT_ON			0
 #define STEP_LIMIT_OFF			1
+
+/**
+ * Read state of digital PIN without turning off PWM.
+ * Original Arduino function also disables PWM on the input pin that affects timers.
+ * @param pin digital pin to read state
+ * @return HIGH or LOW
+ */
+PinStatus digitalReadNoPWM(uint8_t pin)
+{
+	/* Get bit mask and check valid pin */
+	uint8_t bit_mask = digitalPinToBitMask(pin);
+	if (bit_mask == NOT_A_PIN || isDoubleBondedActive(pin)) {
+	    return LOW;
+	}
+	
+	/* Get port and check valid port */
+	PORT_t *port = digitalPinToPortStruct(pin);
+
+	/* Read pin value from PORTx.IN register */
+	if (port->IN & bit_mask){
+		return HIGH;
+	} else {
+		return LOW;
+	}
+}
+
 
 uint8_t LimitSW_Init(void){
     pinMode(S01_PIN, INPUT_PULLUP);
